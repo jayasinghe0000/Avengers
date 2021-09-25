e32a57a83cd3f145d4371c080f6a6b3d19eeba25
IT20240738 <it20240738@my.sliit.lk>
2021-09-25 15:24:17 +0530

    catagory edit

--- app/src/main/java/com/example/avengers/StartActivity.java
+++ app/src/main/java/com/example/avengers/StartActivity.java
@@ -2,13 +2,30 @@ package com.example.avengers;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
+
+    Button asVisitor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        asVisitor = findViewById(R.id.button3);
+
+        asVisitor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(StartActivity.this,CategoryDisplayActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
+
 }
