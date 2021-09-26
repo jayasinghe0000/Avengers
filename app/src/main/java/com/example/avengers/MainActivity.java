10519afc14fb8b33bd06601c0e7be68959cccacf
jayasinghe0000 <it20241650@my.sliit.lk>
2021-09-26 16:29:08 +0530

    Main activity uploaded

---
+++ app/src/main/java/com/example/avengers/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.avengers;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @java.lang.Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
